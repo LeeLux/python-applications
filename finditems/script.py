dee9398294e20a0f8de8595b5881b19873881ce6
LeeLux <104869480+LeeLux@users.noreply.github.com>
2023-03-09 17:34:39 +0100

    Update script.py
    Added support for '.json' and '.mcfunction' file types. All other will be ignored and there wound be any problems when trying to check other file types.

--- finditems/script.py
+++ finditems/script.py
@@ -7,23 +7,33 @@ if not search_string.startswith("minecraft:"):
 
 current_dir = os.getcwd()
 num_found_files = 0
+supported_filetypes = [".json", ".mcfunction"]
 
 def check_file_for_string(file_path):
     global num_found_files
     with open(file_path, "r") as f:
-        data = json.load(f)
-        str_data = json.dumps(data)
-        if search_string in str_data:
+        data = f.read()
+        if file_path.endswith(".json"):
+            try:
+                data = json.loads(data)
+                str_data = json.dumps(data)
+            except:
+                return
+        elif file_path.endswith(".mcfunction"):
+            str_data = data
+        else:
+            return
+        if search_string.lower() in str_data.lower():
             rel_path = os.path.relpath(file_path, current_dir)
-            num_found_files += 1
             print("...\\" + rel_path)
+            num_found_files += 1
 
 def search_dir_for_string(dir_path):
     for root, dirs, files in os.walk(dir_path):
         for file in files:
-            if file.endswith(".json"):
+            if any(file.endswith(ext) for ext in supported_filetypes):
                 file_path = os.path.join(root, file)
                 check_file_for_string(file_path)
 
 search_dir_for_string(current_dir)
-print("Found", num_found_files, "files containing '", search_string, "'.")
+print("Found", num_found_files, "files containing '", search_string.lower(), "'.")
